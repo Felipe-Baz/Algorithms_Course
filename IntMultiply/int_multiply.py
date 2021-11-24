084373f9f7f498a129632a5396d696bf37a93b0d
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2021-11-24 00:27:53 -0300

    :package: Add Karatsuba_Multiplication

--- IntMultiply/int_multiply.py
+++ IntMultiply/int_multiply.py
@@ -1,12 +1,122 @@
+import timeit
+
 #A simple version of the logic behind integer multiplication
-def integer_multiply(a, b):
-    b_list = [int(i) for i in str(b)]
-    b_list.reverse()
+#Author: Felipe Baz Mitsuishi
+def integer_multiply(X, Y):
+    y_list = [int(i) for i in str(Y)]
+    y_list.reverse()
     resultado = 0
-    for index, digit in enumerate(b_list):
-        resultado += (a * digit) * (10**index)
+    for index, digit in enumerate(y_list): 
+        resultado += (X * digit) * (10**index)
     return resultado
 
 
+#A Karatsuba Multiplication
+#Author: Felipe Baz Mitsuishi
+def karatsuba_multiplication(X, Y):
+    #Setup Step:
+    _X = [(i) for i in str(X)]
+    _Y = [(i) for i in str(Y)]
+
+    a = ""
+    b = ""
+    c = ""
+    d = ""
+
+    for index, digit in enumerate(_X):
+        if index < len(_X)/2:
+            a += _X[index]
+        else:
+            b += _X[index]
+    for index, digit in enumerate(_Y):
+        if index < len(_Y)/2:
+            c += _Y[index]
+        else:
+            d += _Y[index]
+    
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+
+    #First Step:
+    ac = a * c
+    #Second Step:
+    bd = b * d
+    #third Step:
+    third_step = (a+b)*(c+d)
+    #fourth Step:
+    fourth_step = third_step - bd - ac
+    #Fifth Step
+    fifth_step = ac* (10 ** 4 ) + bd + fourth_step * (10 ** 2)
+
+    return fifth_step
+
+
+# A Karatsuba Multiplication Using Recursion
+# Code obtained in <"https://pythonandr.com/2015/10/13/karatsuba-multiplication-algorithm-python-code/">
+# Modify by Felipe Baz Mitsuishi
+def karatsuba_recursion_multiplication(x, y):
+    """
+    A Karatsuba Multiplication Using Recursion
+    Code obtained in <"https://pythonandr.com/2015/10/13/karatsuba-multiplication-algorithm-python-code/">
+    Modify by Felipe Baz Mitsuishi
+
+    Parameters:
+    x (int): It is the first number of the multiplication
+    y (int): It is the second number of the multiplication
+
+    Returns:
+    karatsuba_recursion_multiplication(x, y): The result of multiplying the two numbers (x and y)
+    """
+	# Base case of recursion
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        # The Base Case, is an simple multiplication of 2 simple numbers
+	    return x * y
+    # Recursive case
+    else:
+        n = max(len(str(x)),len(str(y)))
+        # Use whole Division to return an int
+        n_divide_by_2 = n // 2
+        # Use whole Division to return an int
+        a = x // 10 ** (n_divide_by_2)
+        # Use the rest to get the other part of the number
+        b = x % 10 ** (n_divide_by_2)
+        # Use whole Division to return an int
+        c = y // 10 ** (n_divide_by_2)
+        # Use the rest to get the other part of the number
+        d = y % 10 ** (n_divide_by_2)
+		
+        # Recursive call
+        ac = karatsuba_recursion_multiplication(a, c)
+        # Recursive call
+        bd = karatsuba_recursion_multiplication(b, d)
+        # Recursive call
+        ad_plus_bc = karatsuba_recursion_multiplication(a + b, c + d) - ac - bd
+        
+        # this little trick, writing n as 2 * n_divide_by_2 takes care of both even and odd n
+        prod = ac * 10 ** (2 * n_divide_by_2) + (ad_plus_bc * 10 ** n_divide_by_2) + bd
+
+        return prod
+
+
 if __name__ == '__main__':
-    print(integer_multiply(1234, 9876))
+    print("-+-"*30)
+    start = timeit.timeit()
+    integer_multiply_response = integer_multiply(5678, 1234)
+    end = timeit.timeit()
+    print(f'Response of Integer Multiply: {integer_multiply_response}')
+    print(f'elapsed time of Integer Multiply: {end - start}')
+    print("-+-"*30)
+    start = timeit.timeit()
+    karatsuba_multiplication_response = karatsuba_multiplication(5678,1234)
+    end = timeit.timeit()
+    print(f'Response of Karatsuba Multiply: {karatsuba_multiplication_response}')
+    print(f'elapsed time of Karatsuba Multiply: {end - start}')
+    print("-+-"*30)
+    start = timeit.timeit()
+    karatsuba_recursive_multiplication_response = karatsuba_recursion_multiplication(5678,1234)
+    end = timeit.timeit()
+    print(f'Response of Karatsuba Recursive Multiply: {karatsuba_recursive_multiplication_response}')
+    print(f'elapsed time of Karatsuba Recursive Multiply: {end - start}')
+    print("-+-"*30)
--- README.md
+++ README.md
@@ -2,5 +2,34 @@
 
 ## Integer Multiply
 
-### First Version - High school
+#### First Version - High school
 
+###### English
+
+The multiplication between integers consists of a simple operation, where 2 numbers will be multiplied (integers [ 0,1,2,...,n]), A and B, if the number B has more than one digit, it will be divided into by digits, thus multiplying each of its digits by the number A, and later multiplying by the power of 10 referring to its decimal place.
+
+###### Português
+
+A multiplicação entre inteiros consiste em uma operação simples, onde 2 números serão multiplicados (inteiros [0,1,2, ..., n]), A e B, se o número B tiver mais de um dígito, será dividido em por dígitos, multiplicando assim cada um de seus dígitos pelo número A, e posteriormente multiplicando pela potência de 10 referente à sua casa decimal.
+
+##### Equation
+
+![](https://i.imgur.com/DDtTEAi.png)
+
+###### English
+
+For the second part of the image, the sum operator runs through all the digits of the number B, multiplying them by A and then by the power of 10 referring to its decimal place.
+
+###### Português
+
+Para segunda parte da imagem, o operador de soma percorre todos os digitos do numero B, multiplicando eles por A e depois pela potencia de 10 referente a sua casa decimal.
+
+##### Big O notation
+
+###### English
+
+This algorithm makes constant &times; N<sup>2</sup> operations, so the big O notation about this algorithm is O(N<sup>2</sup>).
+
+###### Português
+
+Este algoritmo torna contante &times; N<sup>2</sup> operações, então a grande notação O sobre este algoritmo é O(N<sup>2</sup>).
